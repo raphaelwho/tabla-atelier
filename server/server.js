19a3935660c459b5ef379f0492ac9469415537b6
raphaelwho <raphael-lhs@hotmail.com>
2021-05-31 21:51:44 -0500

    add route for server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,72 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+const Token = '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'
 const app = express()
+const axios = require('axios')
 const port = 3000
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./client/dist"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/related', (req, res) => {
+  let config = {
+  method: 'get',
+  url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/22122/related',
+  headers: {
+    'Authorization': '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'
+  }
+  };
+  axios(config)
+  .then(function (response) {
+    //console.log(JSON.stringify(response.data));
+    res.send(response.data)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+})
+
+app.post('/card', (req, res) => {
+  let config = {
+  method: 'get',
+  url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/'+req.body.id,
+  headers: {
+    'Authorization': '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'
+  }
+  };
+
+  axios(config)
+  .then(function (response) {
+    // console.log(JSON.stringify(response.data));
+    res.send(response.data)
+  })
+  .catch(function (error) {
+  console.log(error);
+  });
+
+})
+app.post('/cardimage', (req, res) => {
+  let config = {
+  method: 'get',
+  url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/'+req.body.id+'/styles',
+  headers: {
+    'Authorization': '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'
+  }
+  };
+  axios(config)
+  .then(function (response) {
+    res.send(response.data)
+  })
+  .catch(function (error) {
+  console.log(error);
+  });
+
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
