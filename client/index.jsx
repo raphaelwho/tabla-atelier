8211efcb585c20403ec08305526ae642d5ee482e
raphaelwho <raphael-lhs@hotmail.com>
2021-06-02 12:36:31 -0500

    test

--- client/index.jsx
+++ client/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Square_button from './Square_button/Square_button.jsx';
+import Related from './related/Related.jsx'
 class App extends React.Component {
   constructor() {
     super();
   }
+  handleclick() {
+    alert('Help');
+  }
 
   render() {
-    return <h1>hi</h1>
+
+    return <div>
+    <h1>hi<Square_button Text="ADD TO BAG" width={200} height={50} Click ={this.handleclick}/></h1>
+          <div><Related/></div>
+          </div>
   }
 }
 
