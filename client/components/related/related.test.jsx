c9bcdc6d1ad1c7eb55676d6c43d4b07616cf41b3
raphaelwho <raphael-lhs@hotmail.com>
2021-06-14 20:05:37 -0500

    add test

---
+++ client/components/related/related.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import regeneratorRuntime from "regenerator-runtime";
+// import API mocking utilities from Mock Service Worker
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+// import react-testing methods
+import { render, screen } from '@testing-library/react';
+
+// add custom jest matchers from jest-dom
+import '@testing-library/jest-dom/extend-expect';
+
+// the component to test
+import Related from './Related.jsx'
+
+test('Loads Ratings & Reviews ReviewGraphics Component', async () => {
+  render(<Related id={22128}/>)
+  var ratingReviewHeader = screen.getByText('RELATED PRODUCTS');
+
+  expect(ratingReviewHeader).toBeDefined();
+});
+
+test('Loads Ratings & Reviews ReviewList Component', async () => {
+  render(<Related id={22128} />)
+  var reviewTile = screen.getByText('YOUR OURFIT');
+
+  expect(reviewTile).toBeDefined;
+});
