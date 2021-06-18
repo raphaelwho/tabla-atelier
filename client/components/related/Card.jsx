4cd66ea8e637ffd1e0ca4c468598e43709077501
raphaelwho <raphael-lhs@hotmail.com>
2021-06-17 23:51:22 -0500

    .

--- client/components/related/Card.jsx
+++ client/components/related/Card.jsx
@@ -1,204 +1,160 @@
-import React, { Component } from 'react'
+/* eslint-disable react/prop-types */
+/* eslint-disable require-jsdoc */
+import React, {Component} from 'react';
 import axios from 'axios';
-import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import Comparing from "./Comparing.jsx" ;
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Comparing from './Comparing.jsx';
 import './Card.css';
 
 
 import Stars from '../shared/Stars.jsx';
 
 function SimpleDialog(props) {
-  const { onClose, selectedValue, open,item,cur } = props;
+  const {onClose, selectedValue, open, item, cur} = props;
   const handleClose = () => {
     onClose(selectedValue);
   };
   return (
     <Dialog scroll='paper' onClose={handleClose} open={open} fullWidth
-    maxWidth="md">
+      maxWidth="md">
       <DialogTitle><div>Comparing</div>
-      <div style={{display: 'grid',
-            gridTemplateColumns: '30% 40% 30%'}}>
-      <div>{item.name}</div>
-      <div> </div>
-      <div style={{textAlign: 'right'}}>{cur.name}</div>
-      </div>
+        <div style={{display: 'grid',
+          gridTemplateColumns: '30% 40% 30%'}}>
+          <div>{item.name}</div>
+          <div> </div>
+          <div style={{textAlign: 'right'}}>{cur.name}</div>
+        </div>
 
       </DialogTitle>
       <DialogContent>
-      <Comparing item= {item} cur={cur}/>
+        <Comparing item= {item} cur={cur}/>
       </DialogContent>
     </Dialog>
   );
 }
 
 export default class Card extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state={
       error: null,
-      isLoading : true,
+      isLoading: true,
       item: null,
-      image:null,
-      price:null,
+      image: null,
+      price: null,
       discountPrice: null,
-      display:false,
-      ratings: {}
-    }
+      display: false,
+      ratings: {},
+    };
     this.handleClickOpen=this.handleClickOpen.bind(this);
     this.handlClose=this.handlClose.bind(this);
   }
 
   handlClose() {
-    this.setState({display:false})
+    this.setState({display: false});
   }
 
-  handleClickOpen(){
-      this.setState({display:true})
+  handleClickOpen() {
+    this.setState({display: true});
   }
 
 
   componentDidMount() {
     axios.all([
-      axios.post('http://localhost:3000/card',{id:this.props.item_id}),
-      axios.post('http://localhost:3000/review/meta',{id:this.props.item_id}),
-      axios.post('http://localhost:3000/cardimage',{id:this.props.item_id})
-    ]).then(axios.spread((data1, data2,data3) => {
-      let res = data3.data;
+      axios.post('http://localhost:3000/card', {id: this.props.itemId}),
+      axios.post('http://localhost:3000/review/meta', {id: this.props.itemId}),
+      axios.post('http://localhost:3000/cardimage', {id: this.props.itemId}),
+    ]).then(axios.spread((data1, data2, data3) => {
+      const res = data3.data;
       let cur = res.results[0];
-        this.setState({
-          isLoading : false,
-          item: data1.data,
-          price: data1.data.default_price,
-          image: cur.photos[0].thumbnail_url,
-          ratings: data2.data.ratings,
-          price: cur['original_price'],
-          discountPrice: cur['sale_price']
-        })
-        for (let i=0; i<res.results.length;i++) {
-          cur = res.results[i];
-          // console.log(this.props.id,cur)
-          if (cur['default?']){
-            this.setState({
-              image: cur.photos[0].thumbnail_url,
-              price: cur['original_price'],
-              discountPrice: cur['sale_price']
-            })
-            break
-          }
+      this.setState({
+        isLoading: false,
+        item: data1.data,
+        price: data1.data.default_price,
+        image: cur.photos[0].thumbnail_url,
+        ratings: data2.data.ratings,
+        price: cur['original_price'],
+        discountPrice: cur['sale_price'],
+      });
+      for (let i=0; i<res.results.length; i++) {
+        cur = res.results[i];
+        // console.log(this.props.id,cur)
+        if (cur['default?']) {
+          this.setState({
+            image: cur.photos[0].thumbnail_url,
+            price: cur['original_price'],
+            discountPrice: cur['sale_price'],
+          });
+          break;
         }
-
+      }
     }));
-    // $.ajax({
-    //   url: 'http://localhost:3000/card',
-    //   data: {id:this.props.item_id},
-    //   method: "POST",
-    //   success: (res)=>{
-    //     this.setState({
-    //       isLoading : false,
-    //       item: res,
-    //       price: res.default_price
-    //     })
-    //   }
-    // });
-    // $.ajax({
-    //   url: 'http://localhost:3000/review/meta',
-    //   data: {id:this.props.item_id},
-    //   method: "POST",
-    //   success: (res)=>{
-    //     this.setState({
-    //       ratings: res.ratings
-    //     })
-    //   }
-    // });
-    // $.ajax({
-    //   url: 'http://localhost:3000/cardimage',
-    //   data: {id:this.props.item_id},
-    //   method: "POST",
-    //   success: (res)=>{
-    //     let cur = res.results[0];
-    //     this.setState({
-    //       image: cur.photos[0].thumbnail_url,
-    //       price: cur['original_price'],
-    //       discountPrice: cur['sale_price']
-    //     })
-    //     for (let i=0; i<res.results.length;i++) {
-    //       cur = res.results[i];
-    //       // console.log(this.props.id,cur)
-    //       if (cur['default?']){
-    //         this.setState({
-    //           image: cur.photos[0].thumbnail_url,
-    //           price: cur['original_price'],
-    //           discountPrice: cur['sale_price']
-    //         })
-    //         break
-    //       }
-    //     }
-
-    //   }
-    // });
-
   }
   render() {
     if (this.props.cur === 'blank') {
-      return <div class='card' >
-      <img src = {image} onClick={()=>{this.props.add(this.props.item_id)}} ></img>
-      <div>Add current product!</div>
-    </div>
-
+      return <div className='card' >
+        <img src = {image} onClick={()=>{
+          this.props.add(this.props.item_id);
+        }} ></img>
+        <div>Add current product!</div>
+      </div>;
     }
-    const {error, isLoading, item, image, price, discountPrice, display, ratings } = this.state;
-    const cur = this.props.cur
+    // eslint-disable-next-line max-len
+    const {error, isLoading, item, image, price, discountPrice, display, ratings} = this.state;
+    const cur = this.props.cur;
     let rating = 0;
     let Star;
-      if (Object.keys(ratings).length !== 0) {
-        let numberOfrating=0;
-        let sum = 0;
-        for(let key in ratings) {
+    if (Object.keys(ratings).length !== 0) {
+      let numberOfrating=0;
+      let sum = 0;
+      for (const key in ratings) {
+        if (Object.prototype.hasOwnProperty.call(ratings, key)) {
           numberOfrating += Number(ratings[key]);
-          sum += Number(key) * Number(ratings[key])
+          sum += Number(key) * Number(ratings[key]);
         }
-        rating = sum / numberOfrating;
-        Star=<div><Stars rating = {rating} /></div>;
-      } else {
-        Star = <div></div>
       }
+      rating = sum / numberOfrating;
+      Star=<div><Stars rating = {rating} /></div>;
+    } else {
+      Star = <div></div>;
+    }
 
-      if (error) {
-        return <div>Error: {error} </div> ;
-      } else if (isLoading) {
-        return <div>Loading...</div>;
-      } else {
+    if (error) {
+      return <div>Error: {error} </div>;
+    } else if (isLoading) {
+      return <div>Loading...</div>;
+    } else {
       let displayPrice;
-      if (discountPrice === null )  {
-        displayPrice= <div>${price}</div>
+      if (discountPrice === null ) {
+        displayPrice= <div>${price}</div>;
       } else {
         displayPrice= <div style={{display: 'flex'}}>
-        <div style = {{color: 'red'}}>${discountPrice} </div>
-        <div style ={{textDecoration: 'line-through'}}>  ${price}</div>
-        </div>
+          <div style = {{color: 'red'}}>${discountPrice} </div>
+          <div style ={{textDecoration: 'line-through'}}>  ${price}</div>
+        </div>;
       }
-    return (
+      return (
 
-      <div className='card' >
-        <img src = {image} onClick={this.handleClickOpen} ></img>
-        <div className="icon" onClick={()=>{
+        <div className='card' >
+          <img src = {image} onClick={this.handleClickOpen} ></img>
+          <div className="icon" onClick={()=>{
+            this.props.add();
+          }}>{this.props.icon}</div>
+          <div>{item.category}</div>
+          <div>{item.name}</div>
+          <div>{displayPrice}</div>
+          {Star}
+          <br />
+          <SimpleDialog open={display}
+            onClose={this.handlClose}
+            item={item}
+            cur ={cur}/>
+        </div>
 
-          this.props.add();
 
-        }}>{this.props.icon}</div>
-        <div>{item.category}</div>
-        <div>{item.name}</div>
-        <div>{displayPrice}</div>
-        {Star}
-        <br />
-        <SimpleDialog  open={this.state.display} onClose={this.handlClose} item={item} cur ={cur}/>
-      </div>
-
-
-
-    )
+      );
     }
   }
 }
--- client/components/related/Related.jsx
+++ client/components/related/Related.jsx
@@ -1,93 +1,64 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
+/* eslint-disable react/prop-types */
+/* eslint-disable require-jsdoc */
+import React, {Component} from 'react';
 import Card from './Card.jsx';
 import Swiper from 'react-id-swiper';
 import 'swiper/css/swiper.css';
-import './button.css'
-import {FaRegStar } from 'react-icons/fa';
-import {CgCloseO } from 'react-icons/cg';
+import './button.css';
+import {FaRegStar} from 'react-icons/fa';
+import {CgCloseO} from 'react-icons/cg';
 import axios from 'axios';
 
 export default class Related extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state={
       error: null,
-      isLoading : true,
-      items : [],
-      cur : null,
-      myoutfits:[],
-      rememberMe: false
+      isLoading: true,
+      items: [],
+      cur: null,
+      myoutfits: [],
+      rememberMe: false,
 
-    }
-    this.addToMyoutfits=this.addToMyoutfits.bind(this)
-    this.removeMyOutfit=this.removeMyOutfit.bind(this)
-
+    };
+    this.addToMyoutfits=this.addToMyoutfits.bind(this);
+    this.removeMyOutfit=this.removeMyOutfit.bind(this);
   }
 
   componentDidMount() {
-    let myoutfits = JSON.parse(localStorage.getItem("myoutfits")|| '[]');
-    this.setState({myoutfits})
-
-
+    const myoutfits = JSON.parse(localStorage.getItem('myoutfits')|| '[]');
+    this.setState({myoutfits});
     axios.all([
-      axios.post('http://localhost:3000/card',{id:this.props.id}),
-      axios.post('http://localhost:3000/related',{id:this.props.id})
+      axios.post('http://localhost:3000/card', {id: this.props.id}),
+      axios.post('http://localhost:3000/related', {id: this.props.id}),
     ]).then(axios.spread((data1, data2) => {
       this.setState({
         cur: data1.data,
-        isLoading : false,
-        items: data2.data
-      })
+        isLoading: false,
+        items: data2.data,
+      });
     }));
-
-    // $.ajax({'http://localhost:3000/card',
-    //   url: 'http://localhost:3000/card',
-    //   data: {id:this.props.id},
-    //   method: "POST",
-    //   success: (res)=>{
-    //     this.setState({
-    //       cur: res
-    //     })
-    //   }
-    // })
-
-    // $.ajax({
-    //   url: 'http://localhost:3000/related',
-    //   data: {id:this.props.id},
-    //   method: "POST",
-    //   success: (res)=>{
-    //     this.setState({
-    //       isLoading : false,
-    //       items: res
-    //     })
-    //   }
-    // })
-
-
-
   }
   removeMyOutfit(id) {
-    let myoutfits = [...this.state.myoutfits.filter(item => item !== id)]
-    localStorage.setItem('myoutfits',JSON.stringify(myoutfits) )
-    this.setState({myoutfits})
+    const myoutfits = [...this.state.myoutfits.filter((item) => item !== id)];
+    localStorage.setItem('myoutfits', JSON.stringify(myoutfits) );
+    this.setState({myoutfits});
   }
   addToMyoutfits(id) {
     if (!this.state.myoutfits.includes(id)) {
-      let myoutfits = [...this.state.myoutfits,id]
-      localStorage.setItem('myoutfits',JSON.stringify(myoutfits) )
-      this.setState({myoutfits })
+      const myoutfits = [...this.state.myoutfits, id];
+      localStorage.setItem('myoutfits', JSON.stringify(myoutfits) );
+      this.setState({myoutfits});
     }
   }
 
   render() {
-
-    const {error, isLoading, items, cur,myoutfits } = this.state;
-    log('render',myoutfits)
+    const {error, isLoading, items, cur, myoutfits} = this.state;
+    log('render', myoutfits);
 
 
     if (error) {
-      return <div>Error: {error} </div> ;
+      return <div>Error: {error} </div>;
     } else if (isLoading) {
       return <div>Loading...</div>;
     } else {
@@ -95,37 +66,45 @@ export default class Related extends Component {
         slidesPerView: 4.5,
         navigation: {
           nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
+          prevEl: '.swiper-button-prev',
         },
         observer: true,
-      }
+      };
 
 
-    return (
-      <div>
-        <h1 style={{color:'gray'}}>RELATED PRODUCTS</h1>
-        <Swiper {...params}>
-        {items.map(item_id =>(
-          <div>
-            <Card item_id={item_id} cur={cur} add={()=>this.addToMyoutfits(item_id)} icon={<FaRegStar />}/>
-            </div>
+      return (
+        <div>
+          <h1 style={{color: 'gray'}}>RELATED PRODUCTS</h1>
+          <Swiper {...params}>
+            {items.map((itemId) =>(
+              <div key={itemId}>
+                <Card
+                  itemId={itemId}
+                  cur={cur}
+                  add={()=>this.addToMyoutfits(itemId)}
+                  icon={<FaRegStar/>}/>
+              </div>
             ))}
-        </Swiper>
-        <h1 style={{color:'gray'}}>YOUR OURFIT</h1>
-        <Swiper {...params}>
-        <div >
-          <div onClick={()=>this.addToMyoutfits(cur.id)}>
-          <Card item_id={cur.id} cur={'blank'} add ={()=>{}}/>
-          </div>
+          </Swiper>
+          <h1 style={{color: 'gray'}}>YOUR OURFIT</h1>
+          <Swiper {...params}>
+            <div >
+              <div onClick={()=>this.addToMyoutfits(cur.id)}>
+                <Card item_id={cur.id} cur={'blank'} add ={()=>{}}/>
+              </div>
+            </div>
+            {myoutfits.map( (itemId, index) =>(
+              <div key={itemId}>
+                <Card
+                  itemId={itemId}
+                  cur={cur}
+                  add={()=>this.removeMyOutfit(itemId)}
+                  icon={< CgCloseO />}/>
+              </div>
+            ))}
+          </Swiper>
         </div>
-        {myoutfits.map( (item_id, index) =>(
-            <div>
-            <Card key={item_id}  item_id={item_id} cur={cur} add={()=>this.removeMyOutfit(item_id)} icon={< CgCloseO />}/>
-            </div>
-            ))}
-        </Swiper>
-    </div>
-    )
+      );
     }
   }
 }
