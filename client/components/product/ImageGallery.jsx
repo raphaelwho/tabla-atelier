f68ac70ba6d35a591e2375a34a8d26c458ce6f8d
Andrew Hamilton <andrew@Andrews-MacBook-Pro.local>
2021-06-19 00:10:32 -0700

    Expanding Button

--- client/components/product/ImageGallery.jsx
+++ client/components/product/ImageGallery.jsx
@@ -4,11 +4,18 @@ import './css/ImageGallery.css';
 class ImageGallery extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            expanded: false
+        }
     }
 
     render() {
         return(
-                <img className="featured-image" src='./sample.jpeg' ></img>
+            <div className="img-gallery">
+                <img className="featured-image" src='./sample.jpeg'></img>
+                <h1 className="expand-button">+</h1>
+            </div>
 
         )
     }
