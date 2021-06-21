9848c5ad1872f83e9fbe571eac5485d1bc4165cf
Andrew Hamilton <andrew@Andrews-MacBook-Pro.local>
2021-06-21 17:22:53 -0400

    Complete api working

--- client/components/product/AddToCart.jsx
+++ client/components/product/AddToCart.jsx
@@ -4,12 +4,25 @@ import {Dropdown, DropdownDemo} from '../shared/Dropdown.jsx'
 import CButton from '../shared/CButton.jsx'
 import ClickableStars from '../shared/ClickableStars.jsx'
 
+
+// BAD CODE REFACTOR
 class AddToCart extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            selectedSize: undefined
+            sizes: [],
+            selectedSizeIdx: undefined
+        }
+    }
+
+    componentDidMount() {
+        if (this.props.product) {
+            var sizes = [];
+            for (let [k, v] of Object.entries(this.props.product.skus)) {
+                sizes.push(v.size);
+            }
+            this.setState({sizes});
         }
     }
 
@@ -26,11 +39,24 @@ class AddToCart extends React.Component {
     }
 
     getQuantity() {
-        return 20
+        log("ran");
+        if (this.state.selectedSizeIdx) {
+            var idx = 0;
+            for (let [k, v] of Object.entries(this.props.product.skus)) {
+                log("IDX:", idx, this.state.selectedSizeIdx)
+                if (this.state.selectedSizeIdx === idx + "") {
+                    log("VQuant", v.quantity)
+                    return v.quantity;
+                } 
+                idx++;
+            }
+        } else {
+            return -1;
+        }
     }
 
     selectSize(event) {
-        log(event);
+        this.setState({selectedSizeIdx: event.target.value})
     }
 
     render() {
