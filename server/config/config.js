da4638c4d433f7dd7ae2b96f6167ffe16cdc5c3d
raphaelwho <raphael-lhs@hotmail.com>
2021-06-21 23:01:35 -0500

    fix confict

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-var Git= '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';
-
-module.exports.Git =Git
-=======
 const Git = "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";
-
 module.exports.Git = Git;
->>>>>>> main
